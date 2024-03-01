de966f8d4f073b11daa6af7abdc039599f99d4bc
Vijay Dharap <vijay@kubermatic.com>
2024-03-01 11:45:47 +0530

    Now works for pods and deployments in differnt namespaces

--- README.md
+++ README.md
@@ -6,3 +6,26 @@
 ![Sample screenshot](./screenshot.png)
 
 This is an absolute beginner attempt at such a tool right now. Lets make it better in coming days
+
+## Technologies
+
+1. Python
+1. pip3
+1. Qt for Python
+
+
+## Setup
+
+1. setup venv
+1. pip install pyside6 kubernetes
+1. (optional) vscode with python plugin and .venv environment
+
+## Run
+Save file `kind.kubeconfig` in the current folder. Currently, the kubeconfig must be sourced from fixed file. In future, we will allow it to be selected dynamically / pass the file path as argument.
+```shell
+python kubeclient_ui.py
+```
+
+## Design the UI
+1. Run the designer (.venv\Lib\site-packages\PySide6\designer.exe)
+1. Load file trial-screen.ui
--- kubeclient_ui.py
+++ kubeclient_ui.py
@@ -1,52 +1,116 @@
 import sys
 from PySide6.QtGui import QColor
 from PySide6.QtWidgets import (QApplication, QTableWidget,
-                               QTableWidgetItem)
+                               QTableWidgetItem, QListWidgetItem)
 from PySide6.QtUiTools import QUiLoader
 from PySide6.QtCore import QFile
 from kubernetes import client, config
 
 #TODO: the global variable means we can only have one call at a time?
-pods =[]
+# pods =[]
+# deployments = []
+
+# We should have a tuple .. Name of column and mapped attribute so that whole process can become dynamic
+resouceMapping = {
+    "Pods": {
+        "columns": ["Namespace", "Name", "IP"],
+        "data": []
+    },
+    "Deployments": {
+        "columns": ["Namespace", "Name", "replicas"],
+        "data": []
+    }
+}
+
+# Relevant pod columns and their mapping
+#Labels[]: .metadata.labels
+#Annotations[]: .metadata.annotations
+#Containers[]: .spec.containers
+#InitContainers[]: .spec.init_containers
+#Status??: .status.phase - Can also monitor the .status.conditions[]
+# OR Status:  container_statuses[0].ready / started
+namespaces = []
+
+
+def repopulateTable(resourceType):
+    print(f"list selection changed to {resourceType}")
+    loadTable(window.resourceTable, resourceType)
 
 def populateTable(index):
-    currNamespace = window.comboBox.currentText()
-    print(f"selected index is {index}, {currNamespace}")
+    currNamespace = window.namespaces.currentText()
 
-    table = window.tableWidget
-    table.setColumnCount(3) # FIXME: right now hardcoded at 3
+    table = window.resourceTable
+    resourceType = window.resourceTypeList.currentItem().text()
+    print(f"whats hot: {resourceType}, {index}")
+    cols = resouceMapping[resourceType]['columns']
+    table.setColumnCount(len(cols))
     table.setColumnWidth(1, 340)
-    table.setHorizontalHeaderLabels(["Namespace", "Name", "IP"])
-    table.clear()
-    print(f"about to enumerate {len(pods)}")
-#    # TODO: QTableView is probably better for structured data but needs to define model.
+    table.setRowCount(0)
+    table.setHorizontalHeaderLabels(cols)
+    # print(f"about to enumerate {deployments[0]}")
+    # TODO: QTableView is probably better for structured data but needs to define model.
 
-    # TODO: filter the pods then use filtered collection instead also to set row count
-    table.setRowCount(len(pods))
-    for idx, item in enumerate(pods):
-        print("enumerating")
-        print(f"{currNamespace}, {item.metadata.namespace}")
-        if currNamespace == "ALL" or currNamespace == item.metadata.namespace:
-            print("adding data in row")
-            namespace = QTableWidgetItem(item.metadata.namespace)
-            name = QTableWidgetItem(item.metadata.name)
-            ip = QTableWidgetItem(item.status.pod_ip)
-            table.setItem(idx, 0, namespace)
-            table.setItem(idx, 1, name)
-            table.setItem(idx, 2, ip)
+    if currNamespace != "ALL":
+        filterdList = [o for o in resouceMapping[resourceType]['data'] if currNamespace == o.metadata.namespace]
+    else:
+        filterdList = resouceMapping[resourceType]['data']
+    table.setRowCount(len(filterdList))
+
+    for idx, item in enumerate(filterdList):
+        match resourceType:
+            case "Pods":
+                namespace = QTableWidgetItem(item.metadata.namespace)
+                name = QTableWidgetItem(item.metadata.name)
+                ip = QTableWidgetItem(item.status.pod_ip)
+                table.setItem(idx, 0, namespace)
+                table.setItem(idx, 1, name)
+                table.setItem(idx, 2, ip)
+            case "Deployments":
+                namespace = QTableWidgetItem(item.metadata.namespace)
+                name = QTableWidgetItem(item.metadata.name)
+                replicas = QTableWidgetItem(str(item.status.ready_replicas)+"/"+str(item.status.replicas))
+                table.setItem(idx, 0, namespace)
+                table.setItem(idx, 1, name)
+                table.setItem(idx, 2, replicas)
     table.setSortingEnabled(True)
-    
-def loadTable(table):
-    global pods
-    config.load_kube_config(config_file='./kind.kubeconfig')
-    
-    v1 = client.CoreV1Api()
-    print("Listing pods with their IPs:")
-    ret = v1.list_pod_for_all_namespaces(watch=False)
-    pods = ret.items
-    print("Got list")
-    populateTable(-1)
 
+def loadNS():
+    global namespaces
+    window.namespaces.addItems(["ALL"])
+    v1 = client.CoreV1Api()
+    ret = v1.list_namespace(watch=False)
+    namespaces = ret.items
+    nsIter = map(lambda ns: ns.metadata.name, namespaces)
+    window.namespaces.addItems(nsIter)
+    window.namespaces.currentIndexChanged.connect(populateTable)
+
+
+def loadTable(table, resourceType):
+    global resouceMapping
+    table.setEditTriggers(QTableWidget.NoEditTriggers)
+    v1 = client.CoreV1Api()
+    appsV1 = client.AppsV1Api()
+
+    match resourceType:
+        case "Pods":
+            ret = v1.list_pod_for_all_namespaces(watch=False)
+        case "Deployments":
+            ret = appsV1.list_deployment_for_all_namespaces(watch=False)
+        # case "configmaps":
+        #     print("configmaps")
+        case _:
+            print("Resouce Type NOT IMPLEMETED")
+    resouceMapping[resourceType]['data'] = ret.items
+    populateTable(-1);
+
+def populateResourceTypeList():
+    resouceTypeWidget = window.resourceTypeList
+    QListWidgetItem("Pods", resouceTypeWidget);
+    QListWidgetItem("Deployments", resouceTypeWidget);
+    # QListWidgetItem("ConfigMaps", resouceTypeWidget);
+
+    # in the end - connect to a change event
+    window.resourceTypeList.currentTextChanged.connect(repopulateTable)
 
 
 if __name__ == "__main__":
@@ -57,7 +121,7 @@ if __name__ == "__main__":
         sys.exit(-1)
     print("Loading the ui file into runtime")
     loader = QUiLoader()
-    # Note: Appl must be created AFTER QUiLoader
+    # Note: Application must be created AFTER QUiLoader
     app = QApplication(sys.argv)
     window = loader.load(ui_file, None)
     print("Loading the ui file into runtime completed")
@@ -65,12 +129,13 @@ if __name__ == "__main__":
     if not window:
         print(loader.errorString())
         sys.exit(-1)
+
+    populateResourceTypeList()
     window.show()
-    window.comboBox.addItems(["ALL","default", "kube-system"])
-    window.comboBox.currentIndexChanged.connect(populateTable)
-    loadTable(window.tableWidget)
+
+    # kube api access
+    config.load_kube_config(config_file='./kind.kubeconfig')
+
+    loadNS()
     sys.exit(app.exec())
 
-
-#   sys.exit(app.exec())
-#   
